874b3c56c47dff611bb6b4021fe0c5c9d61fe5f1
Zhuang Chang <zcha621@aucklanduni.ac.nz>
2025-04-12 18:43:32 +1200

    fix: merge from upstream

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -2,16 +2,6 @@
    jQuery plugin settings and other scripts
    ========================================================================== */
 
-<<<<<<< HEAD
-$(document).ready(function(){
-  // These should be the same as the settings in _variables.scss
-  scssLarge = 925; // pixels
-
-  // Sticky footer
-  var bumpIt = function() {
-      $("body").css("margin-bottom", $(".page__footer").outerHeight(true));
-    },
-=======
 $(document).ready(function () {
   // Set the theme on page load
   var setTheme = function (theme) {
@@ -44,57 +34,29 @@ $(document).ready(function () {
   var bumpIt = function () {
     $("body").css("margin-bottom", $(".page__footer").outerHeight(true));
   },
->>>>>>> upstream/master
     didResize = false;
 
   bumpIt();
 
-<<<<<<< HEAD
-  $(window).resize(function() {
-    didResize = true;
-  });
-  setInterval(function() {
-=======
   $(window).resize(function () {
     didResize = true;
   });
   setInterval(function () {
->>>>>>> upstream/master
     if (didResize) {
       didResize = false;
       bumpIt();
     }
   }, 250);
-<<<<<<< HEAD
-  
-=======
-
->>>>>>> upstream/master
   // FitVids init
   fitvids();
 
   // Follow menu drop down
-<<<<<<< HEAD
-  $(".author__urls-wrapper button").on("click", function() {
-    $(".author__urls").fadeToggle("fast", function() {});
-=======
   $(".author__urls-wrapper button").on("click", function () {
     $(".author__urls").fadeToggle("fast", function () { });
->>>>>>> upstream/master
     $(".author__urls-wrapper button").toggleClass("open");
   });
 
   // Restore the follow menu if toggled on a window resize
-<<<<<<< HEAD
-  jQuery(window).on('resize', function() {
-    if ($('.author__urls.social-icons').css('display') == 'none' && $(window).width() >= scssLarge) {
-      $(".author__urls").css('display', 'block')
-    }
-  });    
-
-  // init smooth scroll, this needs to be slightly more than then fixed masthead height
-  $("a").smoothScroll({offset: -65});
-=======
   jQuery(window).on('resize', function () {
     if ($('.author__urls.social-icons').css('display') == 'none' && $(window).width() >= scssLarge) {
       $(".author__urls").css('display', 'block')
@@ -103,7 +65,6 @@ $(document).ready(function () {
 
   // init smooth scroll, this needs to be slightly more than then fixed masthead height
   $("a").smoothScroll({ offset: -65 });
->>>>>>> upstream/master
 
   // add lightbox class to all image links
   $("a[href$='.jpg'],a[href$='.jpeg'],a[href$='.JPG'],a[href$='.png'],a[href$='.gif']").addClass("image-popup");
@@ -115,11 +76,7 @@ $(document).ready(function () {
     gallery: {
       enabled: true,
       navigateByImgClick: true,
-<<<<<<< HEAD
-      preload: [0,1] // Will preload 0 - before current, and 1 after the current image
-=======
       preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
->>>>>>> upstream/master
     },
     image: {
       tError: '<a href="%url%">Image #%curr%</a> could not be loaded.',
@@ -129,11 +86,7 @@ $(document).ready(function () {
     // make it unique to apply your CSS animations just to this exact popup
     mainClass: 'mfp-zoom-in',
     callbacks: {
-<<<<<<< HEAD
-      beforeOpen: function() {
-=======
       beforeOpen: function () {
->>>>>>> upstream/master
         // just a hack that adds mfp-anim class to markup
         this.st.image.markup = this.st.image.markup.replace('mfp-figure', 'mfp-figure mfp-with-anim');
       }
